419a29d81927ce35f3d4bb3fe3dd635f920c82e5
hiepphuc <phuc5411x@gmail.com>
2026-01-03 12:30:22 +0700

    Tối ưu code - Dùng makedirs để tạo đệ quy thư mục nếu thư mục cha chưa có - Chỉ tạo đường dẫn thư mục một lần duy nhất (bỏ ra khỏi vòng lặp)

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 import requests, os
 
+# Cấu hình
 headers = {
     "Referer": "https://www.pixiv.net/",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
 }
-
 artwork_id = 126624328
-response = requests.get(f"https://www.pixiv.net/ajax/illust/{artwork_id}/pages", headers=headers)
+dir_path = "D:/Images/Pixiv Images/"
 
-for img in response.json()["body"]:
-    original_img_url = img["urls"]["original"]
+# Tạo thư mục: dùng makedirs để bao sân các trường hợp thư mục cha chưa có
+os.makedirs(dir_path, exist_ok=True)
 
-    img_original = requests.get(url=original_img_url, headers=headers).content
-    file_name = original_img_url.split("/").pop()
-    dir_path = "D:/Images/Pixiv Images/"
-    full_path = f"{dir_path}{file_name}"
-    if not os.path.exists(dir_path):
-        os.mkdir(dir_path)
-    with open(full_path, mode="wb") as file_img:
-        file_img.write(img_original)
-        print(f"Downloaded image: {original_img_url}")
+# Gọi API
+api_url = f"https://www.pixiv.net/ajax/illust/{artwork_id}/pages"
+response = requests.get(api_url, headers=headers)
+
+# Kiểm tra xem request có thành công không, có thì tải
+if response.status_code == 200:
+    for img in response.json()["body"]:
+        original_img_url = img["urls"]["original"]
+        file_name = original_img_url.split("/").pop()
+        full_path = os.path.join(dir_path, file_name) # Dùng os.path.join để nối đường dẫn chuẩn theo hệ điều hành
+        
+        # Tải ảnh
+        img_content = requests.get(original_img_url, headers=headers).content
+        
+        with open(full_path, mode="wb") as file_img:
+            file_img.write(img_content)
+            print(f"Đã tải xong: {file_name}")
+else:
+    print(f"Lỗi không gọi được API: {response.status_code}")
